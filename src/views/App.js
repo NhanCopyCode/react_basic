08cfe6034d612656acf56043e2a0830ca7307443
thanhnhanggg <lephuocthanhnhat0409@gmail.com>
2024-11-24 16:26:14 +0700

    init project reactjs

---
+++ src/views/App.js
@@ -0,0 +1,23 @@
+import './App.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+			Hello world react class
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React from begining
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
