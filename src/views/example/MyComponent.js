b27aafc2636b6e13d7b33bcdc69ac09cbb64f3fb
thanhnhanggg <lephuocthanhnhat0409@gmail.com>
2024-11-25 12:33:46 +0700

    feat: setState

--- src/views/example/MyComponent.js
+++ src/views/example/MyComponent.js
@@ -2,11 +2,35 @@ import React from "react";
 
 class MyComponent extends React.Component {
     state = {
-        name:"THành Nhân state",
+        name: "THành Nhân state",
+        age: 15,
+    };
+    handleClickButton = (event) => {
+        alert("click me");
+    };
+
+    handleOnChange = (event) => {
+        this.setState({
+            name: event.target.value,
+        })
     }
     render() {
-        const name = "Thành Nhân";
-        return <h1>Hello world, this is class component in reactjs { this.state.name }</h1>;
+        return (
+            <>
+                <h1>
+                    Hello world, this is class component in reactjs
+                    {this.state.name}
+                </h1>
+                <input
+                    value={this.state.name}
+                    type="text"
+                    onChange={(event) => this.handleOnChange(event)}
+                />
+                <button onClick={this.handleOnChange}>
+                    Click me
+                </button>
+            </>
+        );
     }
 }
 
