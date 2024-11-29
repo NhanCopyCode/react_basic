5d60499a8d6bb96bf161b9df29061ad79d24a12f
thanhnhanggg <lephuocthanhnhat0409@gmail.com>
2024-11-29 09:19:08 +0700

    feat: create, delete user by using redux

--- src/store/reducers/rootReducer.js
+++ src/store/reducers/rootReducer.js
@@ -2,11 +2,42 @@ const initState = {
     users: [
         { id: 1, name: "Nhangg" },
         { id: 2, name: "Hoi dan IT" },
+        { id: 3, name: "THanh Nhanggg" },
+        { id: 4, name: "Thanh Huyen" },
     ],
+    testData: "Test thoi nha",
 };
-const rootReducer = (state = initState, action) => {
 
-    return state;
-}
+const rootReducer = (state = initState, action) => {
+    switch (action.type) {
+        case "DELETE_USER":
+            let users = state.users;
+            users = users.filter((item) => item.id !== action.payload.id);
+            return {
+                ...state,
+                users,
+            };
+
+            break;
+        case "CREATE_USER":
+            console.log(">>>>create new user redux", action);
+            const id = Math.floor(Math.random() * 1000);
+            let newUser = {
+                id: id,
+                name: `Thanh Nhangg version ${id}`,
+            };
+
+            return {
+                ...state,
+                users: [...state.users, newUser],
+            };
+            break;
+
+        default:
+            return state;
+
+            break;
+    }
+};
 
 export default rootReducer;
--- src/views/example/Home.js
+++ src/views/example/Home.js
@@ -1,13 +1,56 @@
 import React from "react";
 import { withRouter } from "react-router";
+import { connect } from "react-redux";
 
 class Home extends React.Component {
-    componentDidMount = () => {
-    };
+    componentDidMount = () => {};
+    handleDeleteUser(user) {
+        this.props.deleteUserRedux(user);
+        console.log(this.props);
+    }
+
+    handleAddNewUser() {
+        this.props.createUserRedux();
+    }
     render() {
-        console.log(">>>>> render", this.props);
-        return <div>Home</div>;
+        const listUsers = this.props.dataRedux;
+
+        return (
+            <div>
+                Home
+                {listUsers &&
+                    listUsers.length > 0 &&
+                    listUsers.map((item, index) => {
+                        return (
+                            <div className="mt-3" key={item.id}>
+                                {item.id} - {item.name}
+                                <button
+                                    className="btn btn-danger ms-3"
+                                    onClick={() => this.handleDeleteUser(item)}
+                                >
+                                    Delete User
+                                </button>
+                            </div>
+                        );
+                    })}
+                <button className="btn btn-primary" onClick={() => this.handleAddNewUser()}>Add new User</button>
+            </div>
+        );
     }
 }
 
-export default withRouter(Home);
+const mapStateToProps = (state) => {
+    return {
+        dataRedux: state.users,
+        stateRedux: state,
+    };
+};
+const mapDispatchToProps = (dispatch) => {
+    return {
+        deleteUserRedux: (user) =>
+            dispatch({ type: "DELETE_USER", payload: user }),
+        createUserRedux: () => dispatch({type: 'CREATE_USER'})
+    };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(withRouter(Home));
